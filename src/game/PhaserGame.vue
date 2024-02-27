f97938d491073528668d3f0356ee8a2cdc245bbb
Kihyun Ryu <khryu0610@gmail.com>
2024-02-27 17:50:38 +0900

    Feat: 내 위치 전송 socket통신 구현

--- src/game/PhaserGame.vue
+++ src/game/PhaserGame.vue
@@ -1,64 +1,74 @@
 <script setup lang="ts">
-import { onMounted, onUnmounted, ref } from 'vue';
-import { EventBus } from './EventBus';
-import StartGame from './main';
-import Phaser from 'phaser';
-import io from 'socket.io-client';
+import { onMounted, onUnmounted, ref } from "vue";
+import { EventBus } from "./EventBus";
+import StartGame from "./main";
+import Phaser from "phaser";
+import io from "socket.io-client";
 
 // Save the current scene instance
 const scene = ref();
 const game = ref();
 const player = ref();
-const emit = defineEmits(['current-active-scene','change-scene']);
-let socket = io('http://70.12.246.252:3000');
+const emit = defineEmits(["current-active-scene", "change-scene"]);
+let socket = io("http://70.12.246.252:3000");
 
 export interface BaseSocketData {
     code: number;
-    data : undefined | object    
+    data: undefined | object;
 }
 
 onMounted(() => {
-    game.value = StartGame('game-container');
-    
-    EventBus.on('current-scene-ready', (scene_instance: Phaser.Scene) => {
-        
-        emit('current-active-scene', scene_instance);
-    
+    game.value = StartGame("game-container");
+
+    EventBus.on("current-scene-ready", (scene_instance: Phaser.Scene) => {
+        emit("current-active-scene", scene_instance);
+
         scene.value = scene_instance;
-    
     });
 
-    EventBus.on('start-game', (nickname: string) => {
+    EventBus.on("start-game", (nickname: string) => {
         const param = {
             code: 1001,
             data: {
-                nickname: nickname
-            }
-        }
-        socket.emit('',param, (response: BaseSocketData ) => {
+                nickname: nickname,
+            },
+        };
+        socket.emit("", param, (response: BaseSocketData) => {
             console.log(response);
-            emit('change-scene');
+            emit("change-scene");
         });
-    })
-
-    EventBus.on('change-scene', () => {
-        emit('change-scene');
     });
 
+    EventBus.on("change-scene", () => {
+        emit("change-scene");
+    });
+
+    // 참가자 본인 위치 전송
+    EventBus.on(
+        "player-moved",
+        (startX: number, startY: number, direction: number) => {
+            const param = {
+                code: 2001,
+                data: {
+                    creatureId: "5",
+                    startX,
+                    startY,
+                    direction,
+                },
+            };
+            // socket.emit("", param);
+        }
+    );
 });
 
 onUnmounted(() => {
-
-    if (game.value)
-    {
+    if (game.value) {
         game.value.destroy(true);
         game.value = null;
     }
-
 });
 
 defineExpose({ scene, game });
-
 </script>
 
 <template>
--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -1,98 +1,120 @@
-import { EventBus } from '../EventBus';
-import { Scene } from 'phaser';
+import { EventBus } from "../EventBus";
+import { Scene } from "phaser";
 
 export class Game extends Scene {
     player: Phaser.Physics.Arcade.Sprite;
     cursors: Phaser.Types.Input.Keyboard.CursorKeys;
     platform: Phaser.GameObjects.Image;
     constructor() {
-        super('Game');
+        super("Game");
     }
 
     preload() {
-        this.load.spritesheet('sunfish', 'assets/Sunfish_move.png', { frameWidth: 192, frameHeight: 192 });
-        this.load.image('platform', "assets/bg.png");
+        this.load.spritesheet("sunfish", "assets/Sunfish_move_right.png", {
+            frameWidth: 192,
+            frameHeight: 192,
+        });
+        this.load.image("platform", "assets/bg.png");
     }
 
     create() {
-        this.platform = this.physics.add.staticImage(0, 0, 'platform').setOrigin(0, 0).refreshBody();
+        this.platform = this.physics.add
+            .staticImage(0, 0, "platform")
+            .setOrigin(0, 0)
+            .refreshBody();
         // 스프라이트를 추가합니다.
-        this.player = this.physics.add.sprite(this.cameras.main.centerX, this.cameras.main.centerY, 'sunfish');
+        this.player = this.physics.add.sprite(
+            this.cameras.main.centerX,
+            this.cameras.main.centerY,
+            "sunfish"
+        );
 
         this.player.setCollideWorldBounds(true);
         // 스프라이트에 애니메이션을 추가합니다.
         this.anims.create({
-            key: 'swim',
-            frames: this.anims.generateFrameNumbers('sunfish', { start: 0, end: 1 }),
+            key: "swim",
+            frames: this.anims.generateFrameNumbers("sunfish", {
+                start: 0,
+                end: 1,
+            }),
             frameRate: 3,
-            repeat: -1
+            repeat: -1,
         });
 
         // 스프라이트 애니메이션을 재생합니다.
-        this.player.anims.play('swim');
+        this.player.anims.play("swim");
 
         // 커서 키를 생성합니다.
         if (this.input && this.input.keyboard) {
             this.cursors = this.input.keyboard.createCursorKeys();
         }
 
-        EventBus.emit('current-scene-ready', this);
+        EventBus.emit("current-scene-ready", this);
     }
 
     update() {
-
         const moveSpeed = 5;
         let moveAngle = 0;
-    
+        let direction;
+
         // 대각선 이동 및 회전 각도 결정
         if (this.cursors.left.isDown && this.cursors.up.isDown) {
-            moveAngle = 45; // 왼쪽 위
+            moveAngle = -135; // 왼쪽 위
+            direction = 7;
         } else if (this.cursors.left.isDown && this.cursors.down.isDown) {
-            moveAngle = 315 ; // 왼쪽 아래
+            moveAngle = 135; // 왼쪽 아래
+            direction = 5;
         } else if (this.cursors.right.isDown && this.cursors.up.isDown) {
-            moveAngle = 315; // 오른쪽 위
+            moveAngle = -45; // 오른쪽 위
+            direction = 1;
         } else if (this.cursors.right.isDown && this.cursors.down.isDown) {
             moveAngle = 45; // 오른쪽 아래
+            direction = 3;
         } else if (this.cursors.left.isDown) {
-            //moveAngle = 180; // 왼쪽
+            moveAngle = 180; // 왼쪽
+            direction = 6;
         } else if (this.cursors.right.isDown) {
             moveAngle = 0; // 오른쪽
+            direction = 2;
         } else if (this.cursors.up.isDown) {
-            this.player.scaleX = 1; 
-            moveAngle = 90; // 위
+            this.player.scaleX = 1;
+            moveAngle = -90; // 위
+            direction = 0;
         } else if (this.cursors.down.isDown) {
-            this.player.scaleX = 1; 
-            moveAngle = 270; // 아래
+            this.player.scaleX = 1;
+            moveAngle = 90; // 아래
+            direction = 4;
         }
 
-        
         // 플레이어의 움직임을 처리합니다.
         if (this.cursors) {
-            
             this.player.angle = moveAngle;
             // 왼쪽으로 이동
             if (this.cursors.left.isDown) {
                 this.player.x -= moveSpeed; // 스프라이트를 왼쪽으로 이동
-                this.player.scaleX = 1; // 스프라이트의 기본 방향
-            } 
+                // this.player.scaleX = -1; // 스프라이트의 기본 방향
+            }
             // 오른쪽으로 이동
             else if (this.cursors.right.isDown) {
                 this.player.x += moveSpeed; // 스프라이트를 오른쪽으로 이동
-                this.player.scaleX = -1; // 스프라이트를 좌우로 뒤집음
+                // this.player.scaleX = -1; // 스프라이트를 좌우로 뒤집음
             }
-    
+
             // 위로 이동
             if (this.cursors.up.isDown) {
                 this.player.y -= moveSpeed; // 스프라이트를 위로 이동
-            } 
+            }
             // 아래로 이동
             else if (this.cursors.down.isDown) {
                 this.player.y += moveSpeed; // 스프라이트를 아래로 이동
             }
         }
+
+        // 플레이어의 움직임 결과를 처리합니다.
+        EventBus.emit("player-moved", this.player.x, this.player.y, direction);
     }
 
     changeScene() {
-        this.scene.start('GameOver');
+        this.scene.start("GameOver");
     }
 }
