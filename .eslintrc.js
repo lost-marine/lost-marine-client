6ee120e46b79df5c97d9d4fe36d66506e9ca754a
김은진 <asd0755@naver.com>
2024-02-29 09:18:07 +0900

    Chore: eslint 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,24 @@
-export default {
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
   env: {
     browser: true,
     es2021: true
   },
   extends: ["standard-with-typescript", "plugin:vue/vue3-essential", "prettier"],
-  overrides: [
-    {
-      env: {
-        node: true
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script"
-      }
-    }
-  ],
   parser: "vue-eslint-parser",
   parserOptions: {
     parser: "@typescript-eslint/parser",
     ecmaVersion: "latest",
     sourceType: "module",
-    project: "./tsconfig.app.json"
+    project: ["./tsconfig.json", "./tsconfig.app.json", "./tsconfig.node.json"],
+    extraFileExtensions: [".vue"] // .vue 파일 확장자 추가
   },
   plugins: ["vue"],
   rules: {
-    // "@typescript-eslint/explicit-function-return-type": "off",
-    // "@typescript-eslint/no-unsafe-argument": "off"
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-unsafe-argument": "off"
   }
 };
