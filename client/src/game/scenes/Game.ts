45eb292aa19cf2e52e20cc509e305aa69bd729f2
류기현 <khryu0610@gmail.com>
2024-03-04 15:17:21 +0900

    Feat: 닉네임과 점수가 플레이어 따라다니도록 구현

--- client/src/game/scenes/Game.ts
+++ client/src/game/scenes/Game.ts
@@ -7,6 +7,7 @@ import { directionToAngleFlip } from "../utils/calcs/directionToAngleFlip";
 
 export class Game extends Scene {
   player: Phaser.Physics.Arcade.Sprite;
+  playerContainer: Phaser.GameObjects.Container;
   cursors: Phaser.Types.Input.Keyboard.CursorKeys;
   platform: Phaser.GameObjects.Image;
   direction: DirectionType;
@@ -24,15 +25,9 @@ export class Game extends Scene {
 
   create(): void {
     this.platform = this.physics.add.staticImage(0, 0, "platform").setOrigin(0, 0).refreshBody();
-    // 스프라이트를 추가합니다.
-    this.player = this.physics.add.sprite(
-      // 시작 좌표를 정수로 만듭니다.
-      Math.trunc(this.cameras.main.centerX),
-      Math.trunc(this.cameras.main.centerY),
-      "sunfish"
-    );
 
-    this.player.setCollideWorldBounds(true);
+    // 스프라이트를 추가합니다.
+    this.player = this.physics.add.sprite(0, 0, "sunfish");
     // 스프라이트에 애니메이션을 추가합니다.
     this.anims.create({
       key: "swim",
@@ -43,15 +38,44 @@ export class Game extends Scene {
       frameRate: 3,
       repeat: -1
     });
-
     // 스프라이트 애니메이션을 재생합니다.
     this.player.anims.play("swim");
+
+    // 플레이어 닉네임을 설정합니다.
+    this.player.name = "nickname";
+    const nickname: Phaser.GameObjects.Text = this.add.text(0, 0, this.player.name, {
+      fontSize: "16px",
+      fontStyle: "bold",
+      stroke: "#000",
+      strokeThickness: 1
+    });
+
+    // 플레이어 점수를 설정합니다.
+    const score: Phaser.GameObjects.Text = this.add.text(0, 0, "30", {
+      fontSize: "16px",
+      fontStyle: "bold",
+      stroke: "#000",
+      strokeThickness: 1
+    });
+
+    // 플레이어 스프라이트, 닉네임, 점수를 하나의 컨테이너로 관리합니다.
+    nickname.setPosition(this.player.x - nickname.width / 2, this.player.y - this.player.height / 2 - nickname.height * 2);
+    score.setPosition(this.player.x - score.width / 2, this.player.y - this.player.height / 2 - score.height);
+    this.playerContainer = this.add.container(Math.trunc(this.cameras.main.centerX), Math.trunc(this.cameras.main.centerY), [
+      this.player,
+      nickname,
+      score
+    ]);
+    this.playerContainer.setSize(192, 192);
+    this.physics.world.enable(this.playerContainer);
+    const playerContainerBody = this.playerContainer.body as Phaser.Physics.Arcade.Body;
+    playerContainerBody.setCollideWorldBounds(true);
 
     // 카메라 뷰를 관리합니다.
     this.cameras.main.setBounds(0, 0, 2688, 1536, true);
     this.physics.world.setBounds(0, 0, 2688, 1536);
     // 카메라의 움직임의 부드러운 정도는 startFollow의 3,4번째 인자인 lerp(보간) 값(0~1)을 조정하여 바꿀 수 있습니다.
-    this.cameras.main.startFollow(this.player, true, 0.5, 0.5);
+    this.cameras.main.startFollow(this.playerContainer, true, 0.5, 0.5);
 
     // 커서 키를 생성합니다.
     if (this.input?.keyboard !== null) {
@@ -74,15 +98,18 @@ export class Game extends Scene {
 
     this.player.setFlipX(shouldFlipX);
     this.player.angle = angle;
-    this.player.x += moveSpeed * directionX;
-    this.player.y += moveSpeed * directionY;
+    this.playerContainer.x += moveSpeed * directionX;
+    this.playerContainer.y += moveSpeed * directionY;
+    // const graphics = this.add.graphics();
+    // graphics.lineStyle(2, 0xffffff); // 선 스타일 설정
+    // graphics.strokeRect(this.playerContainer.x, this.playerContainer.y, this.playerContainer.width, this.playerContainer.height);
 
     // 플레이어가 움직일 때만 움직임 결과를 처리합니다.
     const isArrowKeyPressed =
       this.cursors.left.isDown || this.cursors.right.isDown || this.cursors.up.isDown || this.cursors.down.isDown;
 
     if (isArrowKeyPressed) {
-      EventBus.emit("player-moved", this.player.x, this.player.y, this.direction);
+      EventBus.emit("player-moved", this.playerContainer.x, this.playerContainer.y, this.direction);
     }
   }
 
--- client/src/types/direction.ts
+++
@@ -1,4 +0,0 @@
-import type { DIRECTION } from "@/constants/direction";
-
-// 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7
-export type DirectionType = (typeof DIRECTION)[keyof typeof DIRECTION];
