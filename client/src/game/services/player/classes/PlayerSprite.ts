5b6966aba19a76d28c707e14ca94a03cc0e22926
류기현 <khryu0610@gmail.com>
2024-03-05 17:49:25 +0900

    Feat: 점수 업데이트 기능 추가

--- client/src/game/services/player/classes/PlayerSprite.ts
+++ client/src/game/services/player/classes/PlayerSprite.ts
@@ -3,15 +3,17 @@ import { pipe } from "@/game/utils/pipe";
 
 export class PlayerSprite extends Phaser.GameObjects.Container {
   playerId: number;
-  score: number;
+  _point: number;
   // 닉네임과 스코어를 제외한 오직 캐릭터 부분의 sprite만 의미합니다.
   playerSprite: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+  pointSprite: Phaser.GameObjects.Text;
 
   constructor(scene: Phaser.Scene, texture: string, player: Player) {
     super(scene, player.startX, player.startY);
     this.playerId = player.playerId;
     this.name = player.nickname;
-    this.score = 30;
+    // 현재는 undefined를 반환하기 때문에 임시로 0을 넣어두었습니다.
+    this.point = player.point ?? 0;
 
     this.setPosition(Math.trunc(scene.cameras.main.centerX), Math.trunc(scene.cameras.main.centerY));
     // 반드시 Container의 size를 정한 후 scene에 add해야합니다.
@@ -35,7 +37,7 @@ export class PlayerSprite extends Phaser.GameObjects.Container {
       stroke: "#000",
       strokeThickness: 1
     });
-    const score: Phaser.GameObjects.Text = scene.add.text(0, 0, this.score + "", {
+    this.pointSprite = scene.add.text(0, 0, this.point.toString(), {
       fontSize: "16px",
       fontStyle: "bold",
       stroke: "#000",
@@ -46,11 +48,25 @@ export class PlayerSprite extends Phaser.GameObjects.Container {
       this.playerSprite.x - nickname.width / 2,
       this.playerSprite.y - this.playerSprite.height / 2 - nickname.height * 2
     );
-    score.setPosition(this.playerSprite.x - score.width / 2, this.playerSprite.y - this.playerSprite.height / 2 - score.height);
+    this.pointSprite.setPosition(
+      this.playerSprite.x - this.pointSprite.width / 2,
+      this.playerSprite.y - this.playerSprite.height / 2 - this.pointSprite.height
+    );
 
-    this.add([this.playerSprite, nickname, score]);
+    this.add([this.playerSprite, nickname, this.pointSprite]);
 
     const playerContainerBody = this.body as Phaser.Physics.Arcade.Body;
     playerContainerBody.setCollideWorldBounds(true);
+  }
+
+  get point(): number {
+    return this._point;
+  }
+
+  set point(value: number) {
+    this._point = value;
+    if (this.pointSprite !== undefined) {
+      this.pointSprite.setText(this._point.toString());
+    }
   }
 }
--- client/src/game/utils/pipe.ts
+++ client/src/game/utils/pipe.ts
@@ -1,3 +1,8 @@
+/**
+ * 비동기 함수가 아닌 함수들의 실행 순서를 보장해주는 함수입니다.
+ * @param actions 비동기 함수가 아닌 동작들
+ * @returns
+ */
 export const pipe = (...actions: Array<() => any>): boolean => {
   try {
     for (const action of actions) {
