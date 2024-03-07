96070c7bcf2e138713950d7433be8d940838974d
김은진 <asd0755@naver.com>
2024-03-07 10:18:34 +0900

    Fix: 플랑크톤 type 수정

--- client/src/game/types/plankton.ts
+++ client/src/game/types/plankton.ts
@@ -1,11 +1,5 @@
 import type { Creature } from "./creature";
 
 export type Plankton = {
-  width: number;
-  height: number;
   planktonId: number;
-  // health: number;
-  // type: number;
-  // power: number;
-  // status: number;
 } & Creature;
