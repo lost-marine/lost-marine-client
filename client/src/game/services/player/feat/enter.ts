2f154bc5d35eed4ff1758030c4858f663f57cb4f
정서희 <jsh001505@naver.com>
2024-03-13 11:36:34 +0900

    Feat: 게임 시작을 위한 socket 로직 추가

--- client/src/game/services/player/feat/enter.ts
+++ client/src/game/services/player/feat/enter.ts
@@ -10,18 +10,60 @@ type EnterService = {
   onReceviedEnter: (newPlayer: Player) => void;
 };
 
+const nemoUrl = "https://static-image-buckets.s3.ap-northeast-2.amazonaws.com/Nemo.gif";
+const nomalUrl = "https://static-image-buckets.s3.ap-northeast-2.amazonaws.com/NormalFish.gif";
+
 const enterService: EnterService = {
   enterGame: async (nickname: string): Promise<void> => {
     const regexp: RegExp = /^[ㄱ-ㅎㅏ-ㅣ가-힣A-Za-z0-9]{2,12}$/;
     const isValid: boolean = regexp.test(nickname);
+
     if (isValid) {
       socket.emit("nickname-validate", { nickname }, async (response: NameCertificateResponse) => {
         if (response.isSuccess) {
-          socket.emit("player-enter", { nickname, speciesId: 1 }, async (response: NameCertificateResponse) => {
-            await Swal.fire("게임에 입장하는 중", "게임에 입장하고 있습니다. 잠시만 기다려주세요.", "info");
+          await Swal.fire({
+            title: "새로 탄생할 생명을 선택해주세요.",
+            input: "radio",
+            html: `
+            <div style='display : flex; justify-content: space-around'>
+               <div type="submit" onclick="console.log(setSpeciesId); setSpeciesId(1); ">
+                <img src=${nemoUrl} />
+                <h2>흰동가리</h2>
+              </div>
+              <div type="submit" onclick="speciesId=2; console.log(speciesId);">
+                <img height=192 src=${nomalUrl} />
+                <h2>고등어</h2>
+              </div>
+            </div>
+            `,
+            icon: "question",
+            allowEnterKey: false,
+            showCancelButton: true,
+            confirmButtonText: "흰동가리",
+            cancelButtonText: `고등어`
+          }).then((result) => {
+            if (result.isDismissed) {
+              return;
+            }
+            let speciesId: number = 0;
+            if (result.isConfirmed) {
+              speciesId = 1;
+            } else {
+              speciesId = 2;
+            }
+            if (speciesId !== 0) {
+              console.log(speciesId);
+              socket.emit("player-enter", { nickname, speciesId }, async (response: NameCertificateResponse) => {
+                if (response.isSuccess) {
+                  await Swal.fire("게임에 입장하는 중", "게임에 입장하고 있습니다. 잠시만 기다려주세요.", "info");
+                } else {
+                  await Swal.fire("입장 실패", response.msg, "error");
+                }
+              });
+            }
           });
         } else {
-          await Swal.fire("앗!", response.message, "error");
+          await Swal.fire("앗!", response.msg, "error");
         }
       });
     } else {
